79027439b81f8ad0dd685709b4ee8c7a910e397d
wisdompeak <guan.huifeng@gmail.com>
2020-03-14 00:36:53 -0700

    Update 1834.Grouping-Options.cpp

--- DP/1834.Grouping-Options/1834.Grouping-Options.cpp
+++ DP/1834.Grouping-Options/1834.Grouping-Options.cpp
@@ -7,25 +7,18 @@ public:
      */
     long long groupingOptions(int n, int m) 
     {
-        if (n==m) return 1;
-        if (n<m) return 0;
-        
-        long long dp[n+1][m+1] = {0};
-        
-        for (int j=0; j<=n; j++)
+        auto dp = vector<vector<long long>>(n+1, vector<long long>(m+1,0));
+        dp[0][0] = 1;
+        for (int j=1; j<=m; j++)
             dp[0][j] = 1;
-        for (int i=0; i<=n; i++)
-            dp[i][1] = 1;
-            
         
-        for (int i=2; i<=n; i++)
-            for (int j=2; j<=m; j++)
+        for (int i=1; i<=n; i++)
+            for (int j=1; j<=m; j++)
             {
                 dp[i][j] = 0;
                 for (int t=(j==m)?1:0; t*j<=i; t++)
-                    dp[i][j] += dp[i-t*j][j-1];                    
-            }
-        
-        return dp[n][m];
+                    dp[i][j] += dp[i-t*j][j-1];
+            }            
+        return dp[n][m];        
     }
 };
