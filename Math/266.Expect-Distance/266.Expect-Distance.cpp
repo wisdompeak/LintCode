2d4f2de6b321db1d220b9d8442b06934470a4a6a
wisdompeak <guan.huifeng@gmail.com>
2020-08-02 00:29:11 -0700

    Create 266.Expect-Distance.cpp

---
+++ Math/266.Expect-Distance/266.Expect-Distance.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    /**
+     * Return the number of "universal" subarrays.
+     * @param array int整型vector An given array.
+     * @return int整型
+     */
+    int subarrays(vector<int>& array) 
+    {
+        return 2 * x + y + z + 4;
+    }
+};
