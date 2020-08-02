879a42785508dc6c6ad4414f3c99d7e58e1ae4f2
wisdompeak <guan.huifeng@gmail.com>
2020-08-01 17:51:17 -0700

    Update 307.Transform-String.cpp

--- Greedy/307.Transform-String/307.Transform-String.cpp
+++ Greedy/307.Transform-String/307.Transform-String.cpp
@@ -6,33 +6,30 @@ public:
      * @return: return the minimum number of operations to transform
      */
     int transform(string &A, string &B) {
-        int m = A.size(), n = B.size(); 
-        if (n != m) 
-            return -1; 
-        int count[256]; 
-        memset(count, 0, sizeof(count)); 
-        for (int i=0; i<n; i++)   
-            count[B[i]]++; 
-        for (int i=0; i<n; i++)  
-            count[A[i]]--;       
-        for (int i=0; i<256; i++)
-        if (count[i] > 0) 
+        vector<int>count1(256,0);
+        vector<int>count2(256,0);
+        for (int i=0; i<A.size(); i++)   
+            count1[A[i]-'A']++; 
+        for (int i=0; i<B.size(); i++)   
+            count2[B[i]-'A']++;  
+        if (count1!=count2)
             return -1; 
   
         int ret = 0; 
-        for (int i=n-1, j=n-1; i>=0; ) 
+        int i = A.size()-1, j = A.size()-1;
+        while (i>=0) 
         { 
+            while (i >= 0 && A[i] == B[j]) 
+            { 
+                i--; 
+                j--; 
+            }
+            
             while (i>=0 && A[i] != B[j]) 
             { 
                 i--; 
                 ret++; 
             }
-  
-            if (i >= 0) 
-            { 
-                i--; 
-                j--; 
-            } 
         } 
         return ret; 
     }
